9bb728591ec2f8d089a4c264db09de701737210f
yejing678 <1404830922@qq.com>
2024-03-20 12:14:47 +0000

    upload

--- huggingface_downloader/modelscope_downloader.py
+++ huggingface_downloader/modelscope_downloader.py
@@ -1,5 +1,5 @@
-from modelscope.hub.snapshot_download import snapshot_download
+from modelscope import snapshot_download
 
-model_id = "iic/blsp"
+model_id = "iic/blsp_lslm_7b"
 cache_dir = "/home/jye/huggingface/pretrained_model"
 model = snapshot_download(model_id=model_id, cache_dir=cache_dir)
